2f61a1ab40f42d4b345ce694a9264a7654031855
Zhou Yaoyang <diamondzyy@sina.com>
2017-11-11 23:36:38 +0800

    built the framework

--- func-pointer/LLVMAssignment.cpp
+++ func-pointer/LLVMAssignment.cpp
@@ -18,12 +18,20 @@
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/ToolOutputFile.h>
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include <llvm/IR/Module.h>
 
 #include <llvm/Transforms/Scalar.h>
 
 #include <llvm/IR/Function.h>
 #include <llvm/Pass.h>
 #include <llvm/Support/raw_ostream.h>
+#include "llvm/Support/Casting.h"
+
+#include <map>
+#include <list>
 
 #if LLVM_VERSION_MAJOR >= 4
 #include <llvm/Bitcode/BitcodeReader.h>
@@ -32,6 +40,7 @@
 #else
 
 #include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/IR/IntrinsicInst.h>
 
 #endif
 using namespace llvm;
@@ -59,6 +68,7 @@ char EnableFunctionOptPass::ID=0;
 #endif
 
 
+
 ///!TODO TO BE COMPLETED BY YOU FOR ASSIGNMENT 2
 ///Updated 11/10/2017 by fargo: make all functions
 ///processed by mem2reg before this pass.
@@ -66,11 +76,120 @@ struct FuncPtrPass : public ModulePass {
     static char ID; // Pass identification, replacement for typeid
     FuncPtrPass() : ModulePass(ID) {}
 
+    using PossibleFuncList = std::list<Value*>;
+    std::map<llvm::CallInst*, PossibleFuncList> callMap;
+    std::map<llvm::PHINode*, PossibleFuncList> phiMap;
+    std::map<llvm::Function*, PossibleFuncList> functionMap;
 
-    bool runOnModule(Module &M) override {
-        errs() << "Hello: ";
-        errs().write_escaped(M.getName()) << '\n';
-        M.dump();
+    void initMap(Module &module) {
+        // TODO: find all phi of func ptr and function calls
+        for (auto &function: module.getFunctionList()) {
+            functionMap[&function] = PossibleFuncList();
+            for (auto &bb : function) {
+                for (auto &inst : bb) {
+                    if (auto callInst = dyn_cast<CallInst>(&inst)) {
+                        callMap[callInst] = PossibleFuncList();
+                    } else if (auto phi = dyn_cast<PHINode>(&inst)) {
+                        phiMap[phi] = PossibleFuncList();
+                    }
+                }
+            }
+        }
+    }
+
+    template <class T>
+    void printMap(T t) {
+        for (auto &map_it: t) {
+            errs() << *(map_it.first) << "\n";
+            for (auto &list_it: map_it.second) {
+                errs() << list_it << "\n";
+            }
+        }
+    }
+
+    void printMaps() {
+        printMap(functionMap);
+        printMap(callMap);
+        printMap(phiMap);
+    }
+
+    bool iterate(Module &module) {
+        // TODO: update all phi and function calls
+        bool updated = false;
+        for (auto &function: module.getFunctionList()) {
+            for (auto &bb : function) {
+                for (auto &inst : bb) {
+                    if (auto callInst = dyn_cast<CallInst>(&inst)) {
+                        updated = updated || processCall(callInst);
+                    } else if (auto phi = dyn_cast<PHINode>(&inst)) {
+                        updated = updated || processPhi(phi);
+                    }
+                }
+            }
+            updated = updated || processFunction(&function);
+        }
+        return updated;
+    }
+
+    bool processCall(CallInst *callInst) {
+        // TODO: for call, add all phied possible list to possible list
+        // If parameter is func ptr, add corresponding possible list to arg's possible list
+        bool isCallingValue;
+        bool hasFuncPtrArg; // has function pointer arguments
+        bool isReturnFuncPtr;
+
+        return false;
+    }
+
+    bool processPhi(PHINode *phiNode) {
+        // TODO: for phi, add all phied possible list to possible list
+        return false;
+    }
+
+    bool processFunction(Function *function) {
+        // TODO: for functions, if return value is function ptr,
+        return false;
+    }
+
+    bool runOnModule(Module &module) override {
+        errs().write_escaped(module.getName()) << '\n';
+
+        initMap(module);
+
+        while (iterate(module));
+
+#ifdef NEVER_DEFINED
+        for (auto &function: module.getFunctionList()) {
+            for (auto &bb : function) {
+                for (auto &inst : bb) {
+                    errs() << inst.getName() << "||: " << inst.getOpcodeName() << "\n";
+                    if (auto callInst = dyn_cast<CallInst>(&inst)) {
+                        errs() << "Call Inst: =================================\n";
+                        errs() << inst << "\n";
+                        if (DbgValueInst *dbgValueInst = dyn_cast<DbgValueInst>(&inst)) {
+                            errs() << *dbgValueInst << "\n";
+                        } else {
+                            errs() << "Not dbg declare\n";
+                        }
+                        if (Function *calledFunction = callInst->getCalledFunction()) {
+//                            errs() << "Called function: ---------------------------";
+//                            errs() << *calledFunction << "\n";
+
+                        } else {
+                            errs() << "Called function not found\n";
+                            errs() << "Called Value: " << *(callInst->getCalledValue()) << "\n";
+                        }
+                    } else if (auto binOp = dyn_cast<BinaryOperator>(&inst)) {
+                        errs() << "Binary Operation: =================================\n";
+                        errs() << *binOp << "\n";
+                    } else if (auto load = dyn_cast<LoadInst>(&inst)) {
+                        errs() << "Load: =================================\n";
+                        errs() << *load << "\n";
+                    }
+                }
+            }
+        }
+#endif
 
         errs() << "------------------------------\n";
         return false;
