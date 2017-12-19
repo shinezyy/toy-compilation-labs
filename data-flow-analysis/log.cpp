1f93c255ec12237d97c6b9c690d973cd0356f0e2
Zhou Yaoyang <diamondzyy@sina.com>
2017-12-19 11:08:13 +0800

    init for lab3

---
+++ data-flow-analysis/log.cpp
@@ -0,0 +1,15 @@
+bool DEBUG_ALL = false;
+
+bool ArgParse = true;
+
+bool DTSplit = false;
+
+bool MatrixLog = false;
+
+bool VarCompute = false;
+
+bool GBDTLog = true;
+
+bool MGBDTLog = true;
+
+bool MatrixCheck = false;
---
+++ data-flow-analysis/log.h
@@ -0,0 +1,49 @@
+#ifndef __LOG_HPP__
+#define __LOG_HPP__
+
+#include <cstdio>
+#include <cerrno>
+#include <cstring>
+
+// #define LOG_DEBUG
+
+enum log_level { DEBUG = 0, INFO, WARNING, ERROR };
+
+static enum log_level this_log_level = DEBUG;
+
+static const char *log_level_str[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+extern bool DEBUG_ALL;
+
+
+#ifdef LOG_DEBUG
+    #define log_it(fmt, level_str, ...) \
+        fprintf(stderr, "[%s:%u] %s: " fmt  "\n", __FILE__, __LINE__, \
+                level_str, ##__VA_ARGS__);
+#else
+    #define log_it(fmt, level_str, ...) \
+        fprintf(stderr, "%s: " fmt " [%s]\n", level_str, ##__VA_ARGS__, __FUNCTION__);
+#endif
+
+#define log_it_raw(fmt, ...) \
+        fprintf(stderr, fmt , ##__VA_ARGS__);
+
+#define log(level, debug_flags, fmt, ...) \
+    do { \
+        if (DEBUG_ALL && (debug_flags)) {\
+            if (level < this_log_level) \
+            break; \
+            log_it(fmt, log_level_str[level], ##__VA_ARGS__); \
+        }\
+    } while (false)
+
+#define log_raw(level, debug_flags, fmt, ...) \
+    do { \
+        if (DEBUG_ALL && (debug_flags)) {\
+            if (level < this_log_level) \
+            break; \
+            log_it_raw(fmt, ##__VA_ARGS__); \
+        }\
+    } while (false)
+
+#endif
